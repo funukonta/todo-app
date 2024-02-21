5c0330c582889d1b7f0dbbb5f95eae87d55ff575
funukonta <evanroy36@gmail.com>
2024-02-21 08:21:24 +0700

    fix : introduce new main

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/funukonta/todo-app/internal/handler"
 	"github.com/funukonta/todo-app/internal/repository"
 	"github.com/funukonta/todo-app/internal/service"
 	"github.com/funukonta/todo-app/pkg"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
-	db, err := pkg.Connect()
+	// Load Env
+	evnPath := filepath.Join("..", ".env")
+	err := godotenv.Load(evnPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error load .env" + err.Error())
 	}
 
-	mux := http.ServeMux{}
+	db, err := pkg.ConnectPostgre()
+	if err != nil {
+		log.Println(err)
+	}
 
-	repo := repository.NewPostgres(db)
+	repo := repository.NewRepoPostgres(db)
 	service := service.NewTodoService(repo)
 	handler := handler.NewTodoHandler(service)
 
-	mux.HandleFunc("POST /task", handler.CreateTask)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/todo", handler.CreateTask)
+
+	port := ":" + os.Getenv("PORT")
+	log.Println("Server Start at :", port)
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
