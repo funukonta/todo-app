b4ba4edcf5db327b6b00edab18e4737fe4019784
funukonta <evanroy36@gmail.com>
2024-02-21 08:21:08 +0700

    fix : add desc field and adjust datatype

--- internal/model/todo.model.go
+++ internal/model/todo.model.go
@@ -2,12 +2,13 @@ package model
 
 import "time"
 
-type TODO struct {
+type Todo struct {
 	Id        int       `json:"id" db:"id"`
 	TaskName  string    `json:"taskname" db:"taskname"`
+	Desc      string    `json:"desc" db:"desc"`
 	DueDate   time.Time `json:"duedate" db:"duedate"`
-	Priority  string    `json:"priority" db:"priority"`
+	Priority  int       `json:"priority" db:"priority"`
 	Status    bool      `json:"status" db:"status"`
-	CreatedAt time.Time `json:"createdat" db:"createdat"`
-	UpdatedAt time.Time `json:"updatedat" db:"updatedat"`
+	CreatedAt time.Time `json:"createdat,omitempty" db:"createdat"`
+	UpdatedAt time.Time `json:"updatedat,omitempty" db:"updatedat"`
 }
